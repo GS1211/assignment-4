da4bee7743e9478c3015a52a7807f8d1453aa276
GS1211 <manu174303@gmail.com>
2018-08-22 20:00:09 +0530

    Create 2.java
    even no

---
+++ 2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Star
+{
+public static void main(String[] x)
+{
+Scanner a=new Scanner(System.in);
+int i,n;
+n=a.nextInt();
+for(i=0;i<=n;i++)
+{
+if(i%2==0)
+{
+System.out.println(i);}
+
+}
+
+}
+
+
+}
